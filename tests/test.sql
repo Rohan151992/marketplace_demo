a23dc8ae4cbf5ebc2657464102111b528c571ab7
p p <philip@p.com>
2022-04-18 20:42:01 +0000

    aeas

--- tests/test.sql
+++
@@ -1,9 +0,0 @@
-
-    select
-        orders.order_trip_id,
-        orders.customer_id,
-        orders.datestr::date as order_date,
-        orders.order_status,
-        order_payments.total_amount as amount
-
-    from orderstest1333yyyy
