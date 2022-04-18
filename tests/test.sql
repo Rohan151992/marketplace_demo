310c2f0035081cde98c505bafbaec3bc1b21c01b
p p <philip@p.com>
2022-04-18 19:41:39 +0000

    test

---
+++ tests/test.sql
@@ -0,0 +1,9 @@
+
+    select
+        orders.order_trip_id,
+        orders.customer_id,
+        orders.datestr::date as order_date,
+        orders.order_status,
+        order_payments.total_amount as amount
+
+    from orders
